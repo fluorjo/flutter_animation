6f30d74fe940c780bb6017711d88f0089e9ff7b8
fluorjo <fluorjo@naver.com>
2023-08-12 03:07:34 +0900

    4.0 Swiping Gesture

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,7 +2,7 @@ import 'package:animation/screens/apple_watch_screen.dart';
 import 'package:animation/screens/implicit_animations_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animation/screens/explicit_animations_screen.dart';
-
+import 'package:animation/screens/swiping_cards_screen.dart';
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
@@ -50,6 +50,15 @@ class MenuScreen extends StatelessWidget {
                 );
               },
               child: const Text('Apple Watch'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const SwipingCardsScreen(),
+                );
+              },
+              child: const Text('Swiping Cards'),
             ),
           ],
         ),
