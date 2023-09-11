01fae670f5c77c7ce448dbed2122d88a3f5a0c02
fluorjo <fluorjo@naver.com>
2023-09-12 08:13:52 +0900

    CustomPainter

--- lib/screens/apple_watch_screen.dart
+++ lib/screens/apple_watch_screen.dart
@@ -11,9 +11,35 @@ class _AppleWatchScreenState extends State<AppleWatchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
         title: const Text("Apple Watch"),
       ),
+      body: Center(
+        child: CustomPaint(
+          // 커스텀 페인터를 렌더링하고 크기를 정한다.
+          // 그림 그릴 캔버스 영역을 만드는 것.
+          painter: AppleWatchPainter(),
+          size: Size(400, 400),
+        ),
+      ),
     );
+  }
+}
+
+class AppleWatchPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    canvas.drawRect(rect, paint)
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    //true 또는 false를 반환해야 하는 메서드
+    return false;
   }
 }
